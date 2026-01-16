085e191036d94e233caa91fcabc8fd0ca70d6156
Kyoungjin Cho <103394797+kyoungjini@users.noreply.github.com>
2026-01-16 18:17:18 +0900

    [Silver IV] Title: 피하자, Time: 0 ms, Memory: 2020 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/25379. 피하자/피하자.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, res1 = 0, res2 = 0;
+    vector<long long> even_v;
+    vector<long long> odd_v;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        long long tmp;
+        cin >> tmp;
+        (tmp % 2 == 0) ? even_v.push_back(i) : odd_v.push_back(i);
+    }
+
+    // even numbers first
+    int odd_idx = (odd_v.empty() ? 0 : odd_v[0]);
+    for (auto even_i: even_v) {
+        if (even_i > odd_idx) {
+            res1 += (even_i - odd_idx);
+            odd_idx++;
+        }
+    }
+
+    // odd numbers first
+    int even_idx = (even_v.empty() ? 0 : even_v[0]);
+    for (auto odd_i: odd_v) {
+        if (odd_i > even_idx) {
+            res2 += (odd_i - even_idx);
+            even_idx++;
+        }
+    }
+
+    cout << min(res1, res2) << "\n";
+
+    return 0;
+}
