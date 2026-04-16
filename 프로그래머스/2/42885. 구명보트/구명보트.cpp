d45f7de48c86e26bb13be1a13e044e1a10de2889
Kyoungjin Cho <103394797+kyoungjini@users.noreply.github.com>
2026-04-17 00:19:23 +0900

    [level 2] Title: 구명보트, Time: 1.70 ms, Memory: 4.67 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    
+    sort(people.begin(), people.end());
+    
+    int s = 0, e = people.size() - 1;
+    
+    while(s <= e) {
+        if (s == e) {
+            answer++;
+            break;
+        }
+        
+        if (people[s] + people[e] <= limit) {
+            s++;
+        }
+        
+        e--;
+        answer++;
+    }
+    
+    return answer;
+}
